0f795a4156b188fd313ffdc88079fbb7305ef2bd
Justin Wash <justin.wash@pm.me>
2019-05-01 10:21:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,28 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
+const options = [ 
+  'Culinaria :|',
+  'Culinaria :|',
+  'Culinaria :|',
+  'Culinaria :|',
+  'Culinaria :|'
+  'Food Court :)',
+  'Sushi :D',
+  'Sad Sandwiches :(',
+  'Food Trucks :?',
+  'High Pointe :P'
+]
 
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log('I am ready!');
 });
-
- 
 
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+  if (message.content === '@Lunchinator !spin') {
+    var pick = Math.floor((Math.random() * (options.length - 1))
+    message.reply('options[pick]');
+  }
 });
-
- 
-
-// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
