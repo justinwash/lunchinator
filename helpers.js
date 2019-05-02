59c754016887fd90f2cdb9cd8ac35aea0297f457
Justin Wash <justin.wash@pm.me>
2019-05-02 11:05:21 -0500

    i dunno break lunchtime I guess

--- helpers.js
+++ helpers.js
@@ -8,7 +8,7 @@ const showTime = (timestamp) => {
   const MONTH = 1000 * 60 * 60 * 24 * 30
   const YEAR = 1000 * 60 * 60 * 24 * 30 * 12
 
-  const elapsed = timestamp - (5 * 60 * 1000) - new Date().getTime()
+  const elapsed = timestamp - new Date().getTime()
 
   if (elapsed <= 0) return null
 
