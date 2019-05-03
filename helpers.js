e04f6ea9f0dfa36258452e235aa14e673cc964b9
Justin Wash <justin.wash@pm.me>
2019-05-03 10:46:20 -0500

    fix luxon breaking

--- helpers.js
+++ helpers.js
@@ -1,9 +1,10 @@
 const { DateTime } = require("luxon");
 
 const getTimeUntil = (time) => {
-  let lunchTime = DateTime.local({ hour: time.substr(0, 2), minute: time.substr(3, 5) })
+  let lunchTime = DateTime.local().set({ hour: time.substr(0, 2), minute: time.substr(3, 5) })
   let difference = lunchTime.diffNow('minutes');
   console.log(lunchTime.diffNow('minutes'))
+  console.log(difference.toISO())
   return difference.toISO();
 }
 
