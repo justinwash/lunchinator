0aa0966fe7f6ced578a354eec3989db04df360d6
Justin Wash <justin.wash@pm.me>
2019-05-03 10:52:17 -0500

    math

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ const { DateTime } = require("luxon");
 
 const getTimeUntil = (time) => {
   let lunchTime = DateTime.local().set({ hour: time.substr(0, 2), minute: time.substr(3, 5) })
-  let difference = lunchTime.diffNow('minutes');
+  let difference = lunchTime.diffNow('minutes').plus({ hours: 4 });
   console.log(lunchTime.diffNow('minutes'))
   console.log(difference.toObject())
   return difference.toISO();
