e8991607b1ad1885b13388d32e1800da183e5cdd
Justin Wash <justin.wash@pm.me>
2019-05-03 10:49:46 -0500

    remove all emilios code :(

--- helpers.js
+++ helpers.js
@@ -4,21 +4,8 @@ const getTimeUntil = (time) => {
   let lunchTime = DateTime.local().set({ hour: time.substr(0, 2), minute: time.substr(3, 5) })
   let difference = lunchTime.diffNow('minutes');
   console.log(lunchTime.diffNow('minutes'))
-  console.log(difference.toISO())
+  console.log(difference.toObject())
   return difference.toISO();
-}
-
-const getTimeFromMilliseconds = (duration) => {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
 
 module.exports = {
