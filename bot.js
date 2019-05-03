617bb27a0f6d88b348eb9dd6770701f3b1b4bdeb
Justin Wash <justin.wash@pm.me>
2019-05-03 16:04:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,7 +140,8 @@ client.on('message', message => {
     }
   }
 
-  if (message.content.substr(0,10) === '!recursion') {
+  if (message.content.length > 36) message.reply('recursion limited to 20 characters');
+  else if (message.content.substr(0,10) === '!recursion') {
     var index = message.content.substr(11,message.content.length).length
     recurse(index)
   }
