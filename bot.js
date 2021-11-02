98b3ae12ffeded02bbb135d3ca92ecc3dc7ddf2f
Justin Wash <justin.wash@pm.me>
2021-11-02 11:29:59 -0500

    re-enable lunchbot

--- bot.js
+++ bot.js
@@ -8,14 +8,14 @@ var timeForLunch = '11:30'
 const options = [
   // Food choice followed by weighting
   // higher weighting = higher chance to be picked
-  // ['Culinaria :|', 9],
-  // ['Food Court :)', 3],
-  // ['Food Trucks :?', 1],
-  // ['Sushi :D', 1],
-  // ['Sad Sandwiches :purplebeard:', 1],
-  // ['High Pointe :P', 1],
-  // ['ChiliMacs', 1]
-  ['nowhere stay home ya dangus', 100]
+  ['The Fridge', 4],
+  ['Lona\'s', 2],
+  ['Find a Food Truck?', 1],
+  ['Sushi :D', 1],
+  ['Sad Sandwiches', 2],
+  ['Unsad Sandwiches', 1],
+  ['ChiliMacs, why is this still on the list?', 1],
+  ['I dunno DoorDash something', 1]
 ];
 
 var weightSum = 0;
